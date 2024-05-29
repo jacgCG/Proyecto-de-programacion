5c51781ec6c904f4751a5fa105c1eac359e6286a
jacgCG <josuecastrogonzalesxd@gmail.com>
2024-05-28 23:56:25 -0500

    Delete Proyecto de programacion directory

--- Proyecto de programacion/funciones.js
+++
@@ -1,9 +0,0 @@
-const selectCategorias = document.getElementById('select-categorias');
-
-selectCategorias.addEventListener('mouseover', () => {
-  selectCategorias.style.display = 'block';
-});
-
-selectCategorias.addEventListener('mouseout', () => {
-  selectCategorias.style.display = 'none';
-});
